bbfb040934282d85d81598c6c6108791a1376464
Josh Dicker <joshdicker@Joshs-Mac-mini.local>
2025-03-30 12:44:04 -0400

    bonus assignement added

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
-  return (
-    <article>
-      <h3>{title}</h3>
-      <small>{date}</small>
-      <p>{preview}</p>
-    </article>
-  );
-}
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const emoji = minutes < 30 
+      ? "☕️".repeat(Math.ceil(minutes / 5)) 
+      : "🍱".repeat(Math.ceil(minutes / 10));
+  
+    return (
+      <article>
+        <h3>{title}</h3>
+        <small>{date} • {emoji} {minutes} min read</small>
+        <p>{preview}</p>
+      </article>
+    );
+  }
+  
 
 export default Article;
